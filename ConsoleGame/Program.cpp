b16edcbe84b23de99061e2119ae16c1b50bbe029
Maxerdan <48237187+Maxerdan@users.noreply.github.com>
2019-05-20 13:29:07 +0500

    Enemy add

--- ConsoleGame/Program.cpp
+++ ConsoleGame/Program.cpp
@@ -3,18 +3,41 @@
 
 using namespace std;
 // 
+class enemy
+{
+public:
+	virtual int attackGlass() = 0;
+	virtual int attackStick() = 0;
+};
+class policeman : enemy
+{
+public:
+	int xp = 250;
+	int attackGlass() 
+	{
+		cout << "Удар наносит 200 урона полицейскому" << endl;
+		return 200;
+	}
+	int attackStick()
+	{
+		cout << "Удар наносит 50 урона полицейскому" << endl;
+		return 50;
+	}
+};
 class player // stats and inventory
 {
 public:
 	int impudence = 0;
 	bool glass = false;
+	bool glassStat = false;
 	bool stick = false;
+	bool stickStat = false;
 };
 class location
 {
 public:
 	virtual void intro() = 0;
-	virtual void choice(player pl) = 0;
+	virtual void choice() = 0;
 	virtual void first(player pl) = 0;
 	virtual void second(player pl) = 0;
 	virtual void third(player pl) = 0;
@@ -27,9 +50,9 @@ void printStats(player pl)
 	cout << "Статистика:" << endl;
 	cout << "Наглость Тома: " << pl.impudence << endl;
 	cout << "Достижения:" << endl;
-	if (pl.glass)
+	if (pl.glassStat)
 		cout << "Украл стакан из паба" << endl;
-	if (pl.stick)
+	if (pl.stickStat)
 		cout << "Украл палку от швабры из паба" << endl;
 	cout << "\n" << "По мотивам песни:\nСухой закон - Green Crow" << endl;
 	system("pause");
@@ -55,9 +78,9 @@ class end00 : public location
 public:
 	void intro()
 	{
-		cout << "Запален бикфордов шнур, раздался грохот, дверь долой!\nИ пиво вспенилось рекой\nГуляй и пей, веселый люд, и вечных славь богов!\nНарод ликует, пир горой. Ты во главе стола - герой\nНо что-то не дает вздохнуть, метлою тычет" << endl;
+		cout << "Полицейский лежит без сознания, а это значит, что\nЗапален бикфордов шнур, раздался грохот, дверь долой!\nИ пиво вспенилось рекой\nГуляй и пей, веселый люд, и вечных славь богов!\nНарод ликует, пир горой. Ты во главе стола - герой\nНо что-то не дает вздохнуть, метлою тычет" << endl;
 	}
-	void choice(player pl)
+	void choice()
 	{
 		cout << "\nВыберите действие:\n1. Продолжить" << endl;
 	}
@@ -83,7 +106,7 @@ void location7_End00(player pl)
 	while (flag)
 	{
 		pub.intro();
-		pub.choice(pl);
+		pub.choice();
 		int cas;
 		cin >> cas;
 		switch (cas)
@@ -97,31 +120,51 @@ void location7_End00(player pl)
 	}
 }
 
-class Pit : public location
+class Pit
 {
 public:
-	void intro()
+	void intro(policeman pol)
 	{
-		cout << "Один из них оказался твоим корешом - Питом\nОн варит динамит. Тебе в голову приходит мысль взорвать подвал\nПолицейский из паба, из которого ты вышел, следовал за тобой и собирается помешать тебе" << endl;
+		cout << "Здоровье полицейского: " << pol.xp << endl;
 	}
-	void choice(player pl)
-	{
-		if (pl.glass)
-			cout << "\nВыберите действие:\n1. Попытаться напасть толпой на него\n2. Разбить стакан, взятый из паба, о его голову" << endl;
-		else if (pl.stick)
-			cout << "\nВыберите действие:\n1. Попытаться напасть толпой на него\n2. Ударить его палкой" << endl;
-		else 
-			cout << "\nВыберите действие:\n1. Попытаться напасть толпой на него" << endl;
+	void choice00()
+	{		
+		cout << "\nВыберите действие:\n1. Попытаться напасть толпой на него" << endl;
 	}
-	void first(player pl)
+	void choice01()
 	{
-		cout << "Вам не хватило сил одолеть полицейского\nТебя и Пита повязали и забрали в участок" << endl;
-		GameOver(pl);
+		cout << "\nВыберите действие:\n1. Попытаться напасть толпой на него\n2. Кинуть стакан, взятый из паба, ему в голову\n3. Ударить палкой взятой оттуда же" << endl;		
 	}
-	void second(player pl)
+	void choice02()
 	{
-		cout << "Ты повалил полицейского на землю. Кажется он без сознания" << endl;
-		location7_End00(pl);
+		cout << "\nВыберите действие:\n1. Попытаться напасть толпой на него\n2. Кинуть стакан, взятый из паба, ему в голову" << endl;
+	}
+	void choice03()
+	{
+		cout << "\nВыберите действие:\n1. Попытаться напасть толпой на него\n2. Ударить его палкой" << endl;
+	}
+	void first(player pl, policeman pol)
+	{
+		if (pol.xp > 0 && (!pl.glass || !pl.stick)) {
+			cout << "Вам не хватило сил одолеть полицейского\nТебя и Пита повязали и забрали в участок" << endl;
+			GameOver(pl);
+		}
+		else if (pol.xp == 0 || pol.xp < 0)
+			Win(pl);
+		
+
+	}
+	void second00(player pl, policeman &pol)
+	{
+		pol.xp -= pol.attackGlass();		
+		if (pol.xp == 0)
+			location7_End00(pl);
+	}
+	void second01(player pl, policeman &pol)
+	{
+		pol.xp -= pol.attackStick();
+		if (pol.xp == 0)
+			location7_End00(pl);
 	}
 	void third(player pl)
 	{
@@ -129,29 +172,102 @@ public:
 	}
 };
 
-void location6_Pit(player pl)
+void location6_Pit(player pl, policeman pol)
 {
-	Pit pub;
+	Pit pub;	
 	bool flag{ true };
 	while (flag)
 	{
-		pub.intro();
-		pub.choice(pl);
-		int cas;
-		cin >> cas;
-		switch (cas)
+		if (pol.xp < 0 || pol.xp == 0)
+			flag = false;
+		if(pol.xp != 0)
 		{
-		case 1:
-			system("cls");
-			pub.first(pl);
-			flag = false;
-			break;
-		case 2:
-			system("cls");
-			pub.second(pl);
-			flag = false;
-			break;
+		pub.intro(pol);
+			if (!pl.glass && !pl.stick) {
+				pub.choice00();
+				int cas;
+				cin >> cas;
+				switch (cas)
+				{
+				case 1:
+					flag = false;
+					system("cls");
+					pol.xp -= 50;
+					pub.first(pl, pol);
+					break;
+				}
+			}
+		else if (pl.glass && pl.stick) {
+			pub.choice01();
+			int cas;
+			cin >> cas;
+			switch (cas)
+			{
+			case 1:
+				flag = false;
+				system("cls");
+				pol.xp -= 50;
+				pub.first(pl, pol);
+				break;
+			case 2:
+				flag = false;
+				system("cls");
+				pub.second00(pl, pol);
+				pl.glass = false;
+				location6_Pit(pl, pol);
+				break;
+			case 3:
+				flag = false;
+				system("cls");
+				pub.second01(pl, pol);
+				pl.stick = false;
+				location6_Pit(pl, pol);
+				break;
+			}
 		}
+		else if (pl.glass) {
+			pub.choice02();
+			int cas;
+			cin >> cas;
+			switch (cas)
+			{
+			case 1:
+				flag = false;
+				system("cls");
+				pol.xp -= 50;
+				pub.first(pl, pol);
+				break;
+			case 2:
+				flag = false;
+				system("cls");
+				pub.second00(pl, pol);
+				pl.glass = false;
+				location6_Pit(pl, pol);
+				break;
+			}
+		}
+		else if (pl.stick) {
+				pub.choice03();
+				int cas;
+				cin >> cas;
+				switch (cas)
+				{
+				case 1:
+					flag = false;
+					system("cls");
+					pol.xp -= 50;
+					pub.first(pl, pol);
+					break;
+				case 2:
+					flag = false;
+					system("cls");
+					pub.second01(pl, pol);
+					pl.stick = false;
+					location6_Pit(pl, pol);
+					break;
+				}
+			}
+		}		
 	}
 }
 
@@ -162,22 +278,24 @@ public:
 	{
 		cout << "Выйдя на улицу ты сразу же затерялся в толпе людей\nВ городе творится какой-то бардак\nНад ратушей английский флаг\nИ в довершенье этих бед введён сухой закон\nВ утренней газете написано: \"Все пиво заперто в подвал\"" << endl;
 	}
-	void choice(player pl)
+	void choice()
 	{
 		cout << "\nВыберите действие:\n1. Крикнуть на всю улицу в надежде найти своих друзей\n2. Прогуляться по улицам и поискать друзей" << endl;
 	}
 	void first(player pl)
 	{
 		if(pl.impudence == 1)
-		cout << "Ты кричишь во весь голос и с другой стороны улицы тебе кричат в ответ. Это они." << endl;
+		cout << "Ты кричишь во весь голос и с другой стороны улицы тебе кричат в ответ. Это они.\nОдин из них оказался твоим корешом - Питом\nОн варит динамит. Тебе в голову приходит мысль взорвать подвал\nПолицейский из паба, из которого ты вышел, следовал за тобой и собирается помешать тебе" << endl;
 		else if (pl.impudence == 0)
-			cout << "Кажется ты не настолько наглый и постеснялся кричать\nНо не беда. Спустя пару часов ты нашел друзей" << endl;
-		location6_Pit(pl);
+			cout << "Кажется ты не настолько наглый и постеснялся кричать\nНо не беда. Спустя пару часов ты нашел друзей\nОдин из них оказался твоим корешом - Питом\nОн варит динамит. Тебе в голову приходит мысль взорвать подвал\nПолицейский из паба, из которого ты вышел, следовал за тобой и собирается помешать тебе" << endl;
+		policeman pol;
+		location6_Pit(pl, pol);
 	}
 	void second(player pl)
 	{
-		cout << "Спустя пару часов ты находишь друзей" << endl;
-		location6_Pit(pl);
+		cout << "Спустя пару часов ты находишь друзей\nОдин из них оказался твоим корешом - Питом\nОн варит динамит. Тебе в голову приходит мысль взорвать подвал\nПолицейский из паба, из которого ты вышел, следовал за тобой и собирается помешать тебе" << endl;
+		policeman pol;
+		location6_Pit(pl, pol);
 	}
 	void third(player pl)
 	{
@@ -192,7 +310,7 @@ void location5_magazine00(player pl)
 	while (flag)
 	{
 		mag.intro();
-		mag.choice(pl);
+		mag.choice();
 		int cas;
 		cin >> cas;
 		switch (cas)
@@ -211,6 +329,61 @@ void location5_magazine00(player pl)
 	}
 }
 
+class loc2Cont : public location
+{
+public:
+	void intro()
+	{
+		cout << "Проходя мимо двери ты замечаешь неприметную палку от швабры" << endl;
+	}
+	void choice()
+	{
+		cout << "\nВыберите действие:\n1. Забрать с собой\n2. Не брать" << endl;
+	}
+	void first(player pl)
+	{
+		cout << "\"Не пропадать же добру\" - ты подумал" << endl;
+		pl.stick = true;
+		pl.stickStat = true;
+		location5_magazine00(pl);
+	}
+	void second(player pl)
+	{
+		cout << "\"Пожалею что не взял\" - ты подумал" << endl;
+		location5_magazine00(pl);
+	}
+	void third(player pl)
+	{
+
+	}
+};
+
+void location2Continue(player pl)
+{
+	loc2Cont pub;
+	bool flag{ true };
+	while (flag)
+	{
+		pub.intro();
+		pub.choice();
+		int cas;
+		cin >> cas;
+		switch (cas)
+		{
+		case 1:
+			system("cls");
+			pub.first(pl);
+			flag = false;
+			break;
+		case 2:
+			system("cls");
+			pub.second(pl);
+			flag = false;
+			break;
+		}
+	}
+}
+
 class pubExit02 : public location
 {
 public:
@@ -218,7 +391,7 @@ public:
 	{
 		cout << "Но тебя смущает притихший паб, где же “Гиннес”, где же “Харп”?\nЛишь в чашечках дымится чай – здесь вечный файф-о-клок. Сидят английский джентльмен и в каске толстый полисмен\nОни предлагают тебе чашку чая" << endl;
 	}
-	void choice(player pl)
+	void choice()
 	{
 		cout << "\nВыберите действие:\n1. ДА ЗА КОГО ВЫ МЕНЯ ДЕРЖИТЕ?!?! Хлопнуть дверью и уйти\n2. Согласиться" << endl;
 	}
@@ -245,7 +418,7 @@ void location4_PubExit02(player pl)
 	while (flag)
 	{
 		pub.intro();
-		pub.choice(pl);
+		pub.choice();
 		int cas;
 		cin >> cas;
 		switch (cas)
@@ -271,7 +444,7 @@ public:
 	{
 		cout << "" << endl;
 	}
-	void choice(player pl)
+	void choice()
 	{
 		cout << "\nВыберите действие:\n1. Взять палку\n2. Не брать палку" << endl;
 	}
@@ -279,6 +452,7 @@ public:
 	{
 		cout << "Кажется, бармен не заметил этого" << endl;
 		pl.stick = true;
+		pl.stickStat = true;
 		location5_magazine00(pl);
 	}
 	void second(player pl)
@@ -299,7 +473,7 @@ void location3_pubExit01(player pl)
 	while (flag)
 	{
 		pub.intro();
-		pub.choice(pl);
+		pub.choice();
 		int cas;
 		cin >> cas;
 		switch (cas)
@@ -325,26 +499,27 @@ public:
 	{
 		cout << "- Я не знаю, возможно они вышли подышать свежим воздухом, а пока ты не ушел можешь, можешь помочь со стаканами?" << endl;
 	}
-	void choice(player pl)
+	void choice()
 	{
 		cout << "\nВыберите действие:\n1. Послать бармена куда подальше\n2. Отказаться и пойти искать своих друзей на улицу\n3. Украсть стакан и выбежать" << endl;
 	}
 	void first(player pl)
 	{
 		cout << "Бармен прогнал тебя из бара" << endl;
-		location5_magazine00(pl);
+		location2Continue(pl);
 	}
 	void second(player pl)
 	{
 		cout << "- Какой же некультурный человек!" << endl;
-		location5_magazine00(pl);
+		location2Continue(pl);
 	}
 	void third(player pl)
 	{
 		cout << "- А ну стой, подлец!" << endl;
 		pl.glass = true;
+		pl.glassStat = true;
 		pl.impudence++;
-		location5_magazine00(pl);
+		location2Continue(pl);
 	}
 };
 
@@ -355,7 +530,7 @@ void location2_PubExit00(player pl)
 	while (flag)
 	{
 		pub.intro();
-		pub.choice(pl);
+		pub.choice();
 		int cas;
 		cin >> cas;
 		switch (cas)
@@ -386,7 +561,7 @@ public:
 	{
 		cout << "Проснувшись в пабе, ты(Том О’Нил) не можешь вспомнить ничего, кроме того, что ты - Ирландец\nЗамечаешь, что рядом нету твоих друзей, с которыми ты веселился всю ночь, перед тобой, протирает стаканы бармен\nРядом сидят посетители и разговаривают вполголоса" << endl;
 	}
-	void choice(player pl)
+	void choice()
 	{
 		cout << "\nВыберите действие:\n1. Спросить бармена куда делись твои друзья\n2. Пойти искать своих друзей на улицу\n3. Спросить у других посетителей" << endl;
 	}
@@ -415,7 +590,7 @@ void location1_Pub(player pl)
 	{
 		system("cls");
 		pub.intro();
-		pub.choice(pl);
+		pub.choice();
 		int cas;
 		cin >> cas;
 		switch (cas)
@@ -500,5 +675,4 @@ void main()
 {
 	setlocale(LC_ALL, "Russian");
 	location0_Menu();
-
 }
